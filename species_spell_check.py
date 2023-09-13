45eb0f41e6972f5e4f0a888c8e09b98d30624b22
Murphy Potts <mpotts0201@gmail.com>
2023-09-13 13:44:29 -0400

    Removed unneeded comment

--- species_spell_check.py
+++ species_spell_check.py
@@ -5,23 +5,6 @@ from enchant.checker import SpellChecker
 my_dict = DictWithPWL("en_US", "species_dict.txt")
 my_checker = SpellChecker(my_dict)
 
-
-# This is where you would want to brign in a csv/tsv of data to check
-# with open("Taxon.tsv") as fd:
-#     rd = csv.reader(fd, delimiter="\t", quoting=csv.QUOTE_NONE)
-#     # skip first row not to add header name
-#     iterrows = iter(rd)
-#     next(iterrows)
-
-#     # iterate the cloned list with skipped first row
-#     for row in iterrows:
-#         if row[7]:
-#             print("Writing line... " + row[7])
-#             txt_file.writelines(row[7]+'\n')
-
-
-# txt_file.close()
-
 print("Checking...")
 
 print("Suggested spelling: ")
